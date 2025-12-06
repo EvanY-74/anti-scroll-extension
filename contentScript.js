ce93467c3fd30829dcd13f140bd6838d79271ee6
EvanY-74 <138799210+EvanY-74@users.noreply.github.com>
2025-12-06 13:51:48 -0500

    Fixed bug that disabled scrolling on comment section

--- contentScript.js
+++ contentScript.js
@@ -1,12 +1,20 @@
 const observer = new MutationObserver(() => {
-    if (location.pathname.startsWith("/shorts")) document.querySelector('.navigation-container')?.remove();
+    if (!location.pathname.startsWith("/shorts")) return;
+
+    document.querySelector('.navigation-container')?.remove();
+
+    const container = document.getElementById('shorts-container');
+    if (container && !container.dataset.wheelListenerAdded) {
+        container.dataset.wheelListenerAdded = "true";
+
+        container.addEventListener("wheel", e => {
+            if (location.pathname.startsWith("/shorts")) e.preventDefault();
+        }, { passive: false });
+    }
 });
+
 
 observer.observe(document.body, {
     childList: true,
     subtree: true
 });
-
-document.addEventListener("wheel", e => {
-    if (location.pathname.startsWith("/shorts")) e.preventDefault();
-}, { passive: false });
